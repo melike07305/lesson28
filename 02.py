f686a53f157dde8ba9c3c4f03a40662eb8bfcb7c
Melike <m.taganova2007@gmail.com>
2025-02-21 01:00:25 +0500

    Create 02.py

---
+++ 02.py
@@ -0,0 +1,16 @@
+class calculator:
+    def __init__(self,value1,value2):
+        self.value1 = value1
+        self.value2  = value2
+
+    def add_method(self):
+        print("add method: ", self.value! + self.value2 )
+
+
+    def multiply_method(self):
+        print("multiply method: ", self.value1 * self.value2)
+
+numbers = cslculator(10,20)
+
+numbers.add_method()
+numbers.multiply_method()
