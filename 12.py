e9d8d977ee81b1377c0900644cc04e9fe95eed41
Melike <m.taganova2007@gmail.com>
2025-02-21 01:09:27 +0500

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,10 @@
+import datetime
+wagt = datetime.datetime.today()
+
+print("Yyl: ", wagt.year)
+print("Ay: ", wagt.month)
+print("Gun: ", wagt.day)
+print("Sagat: ", wagt.hour)
+print("Minute: ", wagt.minute)
+print("Sekund: ", wagt.second)
+print("Mikrosekund: ", wagt.microsecond)
