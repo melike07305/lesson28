fa269cc0be8c2d9ffb50de4255c8c61bc4e7499f
Melike <m.taganova2007@gmail.com>
2025-02-21 01:03:56 +0500

    Create 04.py

---
+++ 04.py
@@ -0,0 +1,33 @@
+import os
+for i in os.listdir(r"C:\Users\User\Desktop\23_Lesson\Kompyuter5"):
+    print(i)
+
+
+import os
+os.startfile("Screenshot 2025-02-04 223925.png")
+
+
+import os
+os.startfile("Practice_21_1.docx")
+
+
+import os
+os.mkdir("Pictures")
+
+
+import os
+os.rename("Pictures", "Practice")
+
+
+import os
+os.rename("Practice_21_1.docx", "Gujurly")
+
+
+import os
+os.remove("Screenshot 2025-02-04 223925.png")
+
+import os
+os.rmdir(r"C:\Users\User\Desktop\23_Lesson\Kompyuter5\Practice")
+
+
+
